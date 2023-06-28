ee30bfb045de8666f0cc9a5033a9bfd7de965756
Jefferson Valandro <jeffev123@gmail.com>
2023-06-27 22:24:56 -0300

    Ajustado para pegar as ações da API corretamente

--- src/components/ListaAcoes.js
+++ src/components/ListaAcoes.js
@@ -4,13 +4,14 @@ import { MaterialReactTable } from 'material-react-table';
 import { Box, Button } from '@mui/material';
 import { MRT_Localization_PT_BR } from 'material-react-table/locales/pt-BR';
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
-import { ExportToCsv } from 'export-to-csv'; 
+import { ExportToCsv } from 'export-to-csv';
 import { darken } from '@mui/material';
 
 
 const columns = [
   { id: 'ticker', accessorKey: 'ticker', header: 'Ticker', size: 80 },
-  { accessorKey: 'preco', header: 'Preço', size: 80,
+  {
+    accessorKey: 'preco', header: 'Preço', size: 80,
     Cell: ({ cell }) => (
       cell.getValue()?.toLocaleString?.('pt-BR', {
         style: 'currency',
@@ -31,7 +32,8 @@ const columns = [
   { accessorKey: 'rankPl', header: 'Rank Pl', size: 75 },
   { accessorKey: 'somaFormula', header: 'Soma Ranks', size: 85 },
   { accessorKey: 'valorFormulaGrham', header: 'Formula Grham', size: 110 },
-  { accessorKey: 'descontoFormulaGrham', header: 'Desconto Formula Grham', size: 130,
+  {
+    accessorKey: 'descontoFormulaGrham', header: 'Desconto Formula Grham', size: 130,
     Cell: ({ cell }) => (
       <Box
         component="span"
@@ -40,8 +42,8 @@ const columns = [
             cell.getValue() < 0
               ? theme.palette.error.light
               : cell.getValue() >= 0 && cell.getValue() < 30
-              ? theme.palette.warning.light
-              : theme.palette.success.light,
+                ? theme.palette.warning.light
+                : theme.palette.success.light,
           borderRadius: '0.25rem',
           color: '#fff',
           maxWidth: '9ch',
@@ -68,14 +70,17 @@ const csvExporter = new ExportToCsv(csvOptions);
 
 function ListaAcoes() {
   const [lista, setLista] = useState([]);
-  
+
   const handleExportData = () => {
     csvExporter.generateCsv(lista);
   };
 
-
   useEffect(() => {
-    axios.get('http://localhost:8080/api/v1/acoes/')
+    axios.get('http://localhost:8080/api/v1/acoes/', {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+      }
+    })
       .then(response => {
         setLista(response.data);
       })
